896d2b00fb8ac38e47ffb511bbfd08a5aaa0e12c
Sanjay <sanjay.ghosh.connect@gmail.com>
2026-05-14 22:07:50 +0530

    Modifications

---
+++ pair_sum_2pa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> pairSum(vector<int> nums, int target) {
+    vector<int> ans;
+    int n = nums.size();
+
+    int i = 0, j = n-1; 
+
+    while(i < j) {
+        int PairSum = 
+    }
+}
+
+int main() {
+
+    return 0;
+}
