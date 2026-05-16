8ed76fb033d0d026dd177c6319eefdb19967d3cf
Sanjay <sanjay.ghosh.connect@gmail.com>
2026-05-16 16:22:02 +0530

    Modifications

--- pair_sum_2pa.cpp
+++ pair_sum_2pa.cpp
@@ -1,3 +1,5 @@
+// Using 2 Pointers Approach
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -9,11 +11,28 @@ vector<int> pairSum(vector<int> nums, int target) {
     int i = 0, j = n-1; 
 
     while(i < j) {
-        int PairSum = 
+        int PairSum = nums[i] + nums[j];
+        if(PairSum > target) {
+            j--;
+        }
+        else if(PairSum < target) {
+            i++;
+        }
+        else {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
     }
+    return ans;
 }
 
 int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 13;
 
+
+    vector<int> ans = pairSum(nums, target);
+    cout << ans[0] << ", " << ans[1] << endl;
     return 0;
 }
